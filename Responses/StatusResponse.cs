753268ec77a18a9e6fe190b3f2c0357fec405b6f
Moons <me@moons.bio>
2023-11-10 19:11:33 +0100

    feat(routes): add status route

---
+++ Responses/StatusResponse.cs
@@ -0,0 +1,23 @@
+﻿// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+
+namespace API.Responses;
+
+public class Status
+{
+    public ErrorCode ErrorCode { get; set; } = ErrorCode.UnknownError;
+
+    public string ErrorStatus { get; set; } = "Unknown Error";
+
+    public string Message { get; set; } = "Unknown";
+
+    public StatusResponse[]? Response { get; set; }
+}
+
+public class StatusResponse
+{
+    public string Name { get; set; } = "Unknown Monitor";
+
+    public bool Alive { get; set; }
+}
---
+++ Routes/StatusRoute.cs
@@ -0,0 +1,95 @@
+﻿using API.Responses;
+using API.Services;
+using DotNetBungieAPI.Service.Abstractions;
+
+namespace API.Routes;
+
+#pragma warning disable IDE0300
+
+public static class StatusRoute
+{
+    private static readonly ILogger Logger = Logging.CreateLogger("StatusRoute");
+
+    public static void MapStatus(this RouteGroupBuilder group)
+    {
+        group.MapGet("/", async (HttpContext httpContext, IBungieClient bungieClient) =>
+        {
+            var statusResponse = new Status
+            {
+                ErrorCode = ErrorCode.Success,
+                ErrorStatus = "Success",
+                Message = "Felicity.Api.Status",
+                Response = new StatusResponse[]
+                {
+                    new()
+                    {
+                        Name = "Felicity.Api",
+                        Alive = IsFelicityApiAlive(httpContext)
+                    },
+                    new()
+                    {
+                        Name = "Felicity",
+                        Alive = IsFelicityAlive()
+                    },
+                    new()
+                    {
+                        Name = "Bungie",
+                        Alive = await IsBungieAlive(bungieClient)
+                    }
+                }
+            };
+
+            return TypedResults.Json(statusResponse, Common.JsonSerializerOptions);
+        });
+    }
+
+    private static bool IsFelicityApiAlive(HttpContext httpContext)
+    {
+        try
+        {
+            using var client = new HttpClient();
+            var response = client.GetAsync($"{httpContext.Request.Scheme}://{httpContext.Request.Host}/health").Result;
+            return response.IsSuccessStatusCode;
+        }
+        catch (Exception ex)
+        {
+            if (Variables.Environment == Environment.Development)
+                Logger.LogError(ex, "[StatusRoute] {service} is not healthy", "Felicity.Api");
+
+            return false;
+        }
+    }
+
+    private static bool IsFelicityAlive()
+    {
+        try
+        {
+            using var client = new HttpClient();
+            var response = client.GetAsync("http://felicity/health").Result;
+            return response.IsSuccessStatusCode;
+        }
+        catch (Exception ex)
+        {
+            if (Variables.Environment == Environment.Development)
+                Logger.LogError(ex, "[StatusRoute] {service} is not healthy", "Felicity");
+
+            return false;
+        }
+    }
+
+    private static async Task<bool> IsBungieAlive(IBungieClient bungieClient)
+    {
+        try
+        {
+            var response = await bungieClient.ApiAccess.Misc.GetCommonSettings();
+            return response.IsSuccessfulResponseCode && response.Response.Systems["Destiny2"].IsEnabled;
+        }
+        catch (Exception ex)
+        {
+            if (Variables.Environment == Environment.Development)
+                Logger.LogError(ex, "[StatusRoute] {service} is not healthy", "Bungie");
+
+            return false;
+        }
+    }
+}
