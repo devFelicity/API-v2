4482c7cc4b3749c4a7bba9dcabcd2ec0835929f0
Moons <me@moons.bio>
2023-11-11 22:04:47 +0100

    feat(util): add datetime extensions

---
+++ Util/DateTimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿// ReSharper disable UnusedMember.Global
+
+namespace API.Util;
+
+public static class DateTimeExtensions
+{
+    public static DateTime GetRoundedDateTime(int roundMinutes)
+    {
+        var currentTime = DateTime.UtcNow;
+        var minutesToAdd = roundMinutes - currentTime.Minute % roundMinutes;
+        var nextRoundTime = currentTime.AddMinutes(minutesToAdd).AddSeconds(-currentTime.Second);
+        return nextRoundTime;
+    }
+
+    public static TimeSpan GetRoundedTimeSpan(int roundMinutes)
+    {
+        var currentTime = DateTime.UtcNow;
+        var minutesToAdd = roundMinutes - currentTime.Minute % roundMinutes;
+        var nextRoundTime = currentTime.AddMinutes(minutesToAdd).AddSeconds(-currentTime.Second);
+        return nextRoundTime - currentTime;
+    }
+}
